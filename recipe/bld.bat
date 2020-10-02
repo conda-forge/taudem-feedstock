e5cf1027b4e787c32241ac357c9cffd7a8270b8a
Ryan McCarthy <ryan.mccarthy@noaa.gov>
2020-10-02 08:00:14 -0700

    adding env vars for inc and lib paths for msbuild

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,11 @@ if "%ARCH%"=="32" (
 
 cd Taudem5PCVS2015
 
+rem Setting Include/Lib paths
+set "INCLUDE=%LIBRARY_INC%;%INCLUDE%"
+set "LIB=%LIBRARY_LIB%;%LIB%"
+set UseEnv=true
+
 msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release
 
 for /r %SRC_DIR%\out\Release-%PLATFORM%\bin %%f in (*.exe) do @copy "%%f" %LIBRARY_BIN%
